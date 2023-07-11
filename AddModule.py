31d926c78b0d432528c90437c3e6989486790c92
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-07-11 19:31:27 +0530

    program

---
+++ AddModule.py
@@ -0,0 +1,18 @@
+def addition(num1,num2):
+    #num1=int(input("Enter first number :"))
+    #num2=int(input("Enter Second Number :"))
+    result=num1+num2
+    return result
+    #print("Addition :",result)
+
+def multiply():
+    num1=int(input("Enter first number :"))
+    num2=int(input("Enter Second Number :"))
+    result=num1*num2
+    print("multiplication :",result)
+
+def Division():
+    num1=int(input("Enter first number :"))
+    num2=int(input("Enter Second Number :"))
+    result=num1/num2
+    print("Division :",result)
---
+++ callModule.py
@@ -0,0 +1,13 @@
+#import AddModule as add
+
+#from AddModule import addition,multiply
+from AddModule import *
+x=int(input("Enter num1:"))
+y=int(input("Enter num2:"))
+r=addition(x,y)
+print("Sum is :",r)
+
+
+
+#multiply()
+#Division()
